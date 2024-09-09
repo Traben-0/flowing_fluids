18bbaa0aaa67a1e6af223a06c310ff875dfdba61
Traben <ben.trabant@gmail.com>
2024-09-10 02:37:02 +1000

    random ticks

--- common/src/main/java/traben/waterly/mixin/MixinWaterFluid.java
+++ common/src/main/java/traben/waterly/mixin/MixinWaterFluid.java
@@ -6,6 +6,7 @@ import net.minecraft.tags.BiomeTags;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.LightLayer;
 import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.material.*;
@@ -14,19 +15,22 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import traben.waterly.FluidGetterByAmount;
 
+import java.util.Random;
+
 
 @Mixin(WaterFluid.class)
 public abstract class MixinWaterFluid extends FlowingFluid implements FluidGetterByAmount {
 
     @Shadow public abstract int getDropOff(final LevelReader levelReader);
 
-
+    @Unique
+    private Random waterly$random = new Random();
 
     @Override
     protected void randomTick(final Level level, final BlockPos blockPos, final FluidState fluidState, final RandomSource randomSource) {
         super.randomTick(level, blockPos, fluidState, randomSource);
 
-        if(true && !fluidState.isEmpty()){
+        if(true && !fluidState.isEmpty() && waterly$random.nextInt(100) == 0){//todo setting for tick rate and tweak
             int amount = fluidState.getAmount();
             if (!waterly$increase(level, blockPos, amount))
                 waterly$decrease(level, blockPos, amount);
@@ -41,15 +45,17 @@ public abstract class MixinWaterFluid extends FlowingFluid implements FluidGette
 
     @Unique
     private boolean waterly$increase(final Level level, final BlockPos blockPos, int amount){
-        if (amount < 8 && level.canSeeSky(blockPos)) {
-            if (level.isRaining()) {//can see sky and raining
+
+        if (amount < 8) {
+            if (level.isRaining() && level.canSeeSky(blockPos)) {//can see sky and raining
                 level.setBlockAndUpdate(blockPos, waterly$getOfAmount(/*level, blockPos, level.getBlockState(blockPos),*/ amount + 1).createLegacyBlock());
                 return true;
             }
             //if in ocean or river and below or at sea level and above 0
             var biome = level.getBiome(blockPos);
-            if (level.getSeaLevel() >= blockPos.getY()
-                    && (biome.is(BiomeTags.IS_OCEAN)
+            if (level.getSeaLevel() >= blockPos.getY()//between sea level and 0
+                    && level.getBrightness(LightLayer.SKY,blockPos) > 0 // is close enough to sky/atmosphere access
+                    && (biome.is(BiomeTags.IS_OCEAN)// is biome with refilling water
                     || biome.is(BiomeTags.IS_RIVER)
                     || biome.is(BiomeTags.IS_BEACH)
                     || biome.is(Biomes.SWAMP)
@@ -63,7 +69,10 @@ public abstract class MixinWaterFluid extends FlowingFluid implements FluidGette
 
     @Unique
     private void waterly$decrease(final Level level, final BlockPos blockPos, int amount){
-        if(amount > 0 && amount <= getDropOff(level)){
+        //evaporate over time if exposed to any sky light
+        if(amount > 0 && amount <= getDropOff(level)
+                && level.getFluidState(blockPos.below()).isEmpty()
+                && level.getBrightness(LightLayer.SKY,blockPos) > 0){//todo setting
             level.setBlockAndUpdate(blockPos, Blocks.AIR.defaultBlockState());
         }
     }
