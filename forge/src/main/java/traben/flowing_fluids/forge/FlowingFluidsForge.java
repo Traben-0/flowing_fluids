973d3044c98ab3175018a55e36226f73ec9e08f6
Traben <ben.trabant@gmail.com>
2024-09-21 18:30:14 +1000

    rename and added forge

--- forge/src/main/java/traben/flowing_fluids/forge/FlowingFluidsForge.java
+++ forge/src/main/java/traben/flowing_fluids/forge/FlowingFluidsForge.java
@@ -1,11 +1,12 @@
 package traben.flowing_fluids.forge;
 
+import net.minecraftforge.fml.common.Mod;
 import traben.flowing_fluids.FlowingFluids;
 
 
 @Mod(FlowingFluids.MOD_ID)
-public final class FlowingFluidsNeoForge {
-    public FlowingFluidsNeoForge() {
+public final class FlowingFluidsForge {
+    public FlowingFluidsForge() {
         // Run our common setup.
         FlowingFluids.init();
     }
