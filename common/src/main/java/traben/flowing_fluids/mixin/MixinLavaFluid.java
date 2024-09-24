d518def238bfe0823103732e2cc60334b9728d4c
Traben <ben.trabant@gmail.com>
2024-09-25 01:09:19 +1000

    fix lava fizzing when replacing lava

--- common/src/main/java/traben/flowing_fluids/mixin/MixinLavaFluid.java
+++ common/src/main/java/traben/flowing_fluids/mixin/MixinLavaFluid.java
@@ -4,22 +4,36 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.material.LavaFluid;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import traben.flowing_fluids.FlowingFluids;
 
 @Mixin(LavaFluid.class)
-public class MixinLavaFluid {
+public abstract class MixinLavaFluid {
+
+    @Shadow public abstract boolean isSame(final Fluid fluid);
 
     @Inject(method = "canBeReplacedWith", at = @At(value = "RETURN"), cancellable = true)
     private void flowing_fluids$removeHeightCheck(final FluidState fluidState, final BlockGetter blockGetter, final BlockPos blockPos, final Fluid fluid, final Direction direction, final CallbackInfoReturnable<Boolean> cir) {
         if (FlowingFluids.config.enableMod && !cir.getReturnValue()) {
-            cir.setReturnValue(fluid.is(FluidTags.WATER));
+            cir.setReturnValue(fluid.isSame(Fluids.WATER));
+        }
+    }
+
+    @Inject(method = "beforeDestroyingBlock", at = @At(value = "HEAD"), cancellable = true)
+    private void ff$fizzOnlyForNonLava(final LevelAccessor level, final BlockPos pos, final BlockState state, final CallbackInfo ci) {
+        if (FlowingFluids.config.enableMod && isSame(state.getFluidState().getType())) {
+            ci.cancel();
         }
     }
 }
